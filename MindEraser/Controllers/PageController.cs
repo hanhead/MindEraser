60c6322e0a139d02708bfa2b749ed6c0c80aa524
hanhead1227 <74334952+hanhead1227@users.noreply.github.com>
2021-05-03 23:21:03 -0700

    check in project pages

---
+++ MindEraser/Controllers/PageController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MindEraser.Controllers
+{
+    public class PageController : Controller
+    {
+        // GET: Page
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult A()
+        {
+            return View();
+        }
+        public ActionResult B()
+        {
+            return View();
+        }
+        public ActionResult C()
+        {
+            return View();
+        }
+        public ActionResult D()
+        {
+            return View();
+        }
+    }
+}
